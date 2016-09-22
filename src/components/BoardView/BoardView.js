1649bff50eb7bb62057ea93992e89b83a1818f98
Clint Goodman <goodmancf@ldschurch.org>
2016-09-21 21:20:14 -0600

    adding the board title to view

--- src/components/BoardView/BoardView.js
+++ src/components/BoardView/BoardView.js
@@ -9,11 +9,12 @@ class BoardView extends React.Component {
         super(props);
         this.state = {
             collections: [],
-            id: this.props.params.boardId
+            id: this.props.params.boardId,
+            title: 'Title coming soon...'
         };
     }
 
-    updateFromFB(snapshot) {
+    updateCollectionsFromFB(snapshot) {
         if (snapshot.exists()){
             this.state = {
                 collections: Object.keys(snapshot.val()),
@@ -26,22 +27,35 @@ class BoardView extends React.Component {
         }
     }
 
+    updateBoardFromFB(snapshot) {
+        if (snapshot.exists()){
+            this.state.title = snapshot.val().title;
+            this.setState(this.state);
+        } else {
+            this.state.title = '';
+            this.setState(this.state);
+        }
+    }
+
     componentDidMount() {
         var boardId = this.props.params.boardId;
-        myFirebaseRef.child("collections").orderByChild("board/" + boardId).equalTo(boardId).on("value", this.updateFromFB.bind(this));
+        myFirebaseRef.child("collections").orderByChild("board/" + boardId).equalTo(boardId).on("value", this.updateCollectionsFromFB.bind(this));
+        myFirebaseRef.child("boards/" + boardId).on("value", this.updateBoardFromFB.bind(this));
     }
 
     componentWillUnmount() {
         var boardId = this.props.params.boardId;
-        myFirebaseRef.child("collections").orderByChild("board/" + boardId).equalTo(boardId).off("value", this.updateFromFB.bind(this));
+        myFirebaseRef.child("collections").orderByChild("board/" + boardId).equalTo(boardId).off("value", this.updateCollectionsFromFB.bind(this));
+        myFirebaseRef.child("boards/" + boardId).off("value", this.updateBoardFromFB.bind(this));
     }
 
     render() {
         var collections = this.state.collections.map((collection, index) => {
             return <CollectionCard boardId={this.state.id} id={collection} key={index}/>;
         });
+        var { title } = this.state;
         return <section className="collectionRow">
-            <h1 className="title">{this.state.id}</h1>
+            <h1 className="title">{title}</h1>
             {collections}
             <CollectionCardEditable boardId={this.state.id}/>
         </section>;
