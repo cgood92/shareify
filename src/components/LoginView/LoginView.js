159edd67927da00a5a5c5a7d52ff983a0c99242f
Clint Goodman <goodmancf@ldschurch.org>
2016-09-21 21:01:31 -0600

    making the login look prettier

--- src/components/LoginView/LoginView.js
+++ src/components/LoginView/LoginView.js
@@ -86,40 +86,51 @@ class LoginView extends React.Component {
   render() {
     return (
       <div className="authentication">
-          <form className="login">
-               <h2>Login</h2>
-
-              <div className="form-group">
-                  <label htmlFor="txtEmail">Email address</label>
-                  <input type="email" className="form-control" id="txtEmail" placeholder="Enter email" name="email" />
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <div className="card blue-grey darken-2">
+              <div className="card-content white-text">
+                 <h2>Login</h2>
+                <form className="login">
+                    <div className="form-group">
+                        <label htmlFor="txtEmail">Email address</label>
+                        <input type="email" className="form-control" id="txtEmail" placeholder="Enter email" name="email" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="txtPass">Password</label>
+                        <input type="password" className="form-control" id="txtPass" placeholder="Password" name="password" />
+                    </div>
+                    <button type="submit" className="btn btn-default btn-block login__button">Login</button>
+                     <span className="login__other">OR you can login with: 
+                       <ul className="login__socialButtons">
+                        <li><a className="btn btn-primary bt-social" data-provider="facebook" onClick={this.handleThirdPartyLogin.bind(this, 'Facebook')}>Facebook</a></li>
+                        <li><a className="btn btn-primary bt-social" data-provider="google" onClick={this.handleThirdPartyLogin.bind(this, 'Google')}>Google</a></li>
+                        <li><a className="btn btn-info bt-social" data-provider="twitter" onClick={this.handleThirdPartyLogin.bind(this, 'Twitter')}>Twitter</a></li>
+                        <li><a className="btn btn-default bt-social" data-provider="github" onClick={this.handleThirdPartyLogin.bind(this, 'Github')}>GitHub</a></li>
+                       </ul>
+                     </span>
+                </form>
               </div>
-              <div className="form-group">
-                  <label htmlFor="txtPass">Password</label>
-                  <input type="password" className="form-control" id="txtPass" placeholder="Password" name="password" />
+            </div>
+            <div className="card blue-grey darken-3">
+              <div className="card-content white-text">
+                <form className="register" id="register" onSubmit={this.register.bind(this)}> 
+                   <h2>Register</h2>
+
+                  <div className="form-group">
+                      <label htmlFor="txtRegEmail">Email address</label>
+                      <input type="email" className="form-control" id="txtEmail" placeholder="Enter email" name="email" />
+                  </div>
+                  <div className="form-group">
+                      <label htmlFor="txtRegPass">Password</label>
+                      <input type="password" className="form-control" id="txtPass" placeholder="Password" name="password" />
+                  </div>
+                  <button type="submit" className="btn btn-default">Register</button>
+                </form>
               </div>
-              <button type="submit" className="btn btn-default btn-block">Login</button>
-              <br />
-              <br />
-               <h4>Login with</h4>
-          <a className="btn btn-primary bt-social" data-provider="facebook" onClick={this.handleThirdPartyLogin.bind(this, 'Facebook')}>Facebook</a>
-          <a className="btn btn-primary bt-social" data-provider="google" onClick={this.handleThirdPartyLogin.bind(this, 'Google')}>Google</a>
-          <a className="btn btn-info bt-social" data-provider="twitter" onClick={this.handleThirdPartyLogin.bind(this, 'Twitter')}>Twitter</a>
-          <a className="btn btn-default bt-social" data-provider="github" onClick={this.handleThirdPartyLogin.bind(this, 'Github')}>GitHub</a>
-
-          </form>
-          <form className="register" id="register" onSubmit={this.register.bind(this)}> 
-             <h2>Register</h2>
-
-            <div className="form-group">
-                <label htmlFor="txtRegEmail">Email address</label>
-                <input type="email" className="form-control" id="txtEmail" placeholder="Enter email" name="email" />
             </div>
-            <div className="form-group">
-                <label htmlFor="txtRegPass">Password</label>
-                <input type="password" className="form-control" id="txtPass" placeholder="Password" name="password" />
-            </div>
-            <button type="submit" className="btn btn-default">Register</button>
-          </form>
+          </div>
+        </div>
       </div>
     );
   }
